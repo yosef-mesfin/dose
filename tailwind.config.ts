5641b2efc0b82c4eccee14536e1b536ac356a0c5
joe <yosefmesfin89@gmail.com>
2024-07-28 02:12:30 +0300

    ci: implement custom animation for spinning border

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -70,10 +70,16 @@ const config: Config = {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
+        'border-spin': {
+          '100%': {
+            transform: 'rotate(-360deg)',
+          },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'border-spin': 'border-spin 8s linear infinite',
       },
     },
   },
